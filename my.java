c4abfb9d83a60967f40e5442dd26d9096310a7ac
Maroti Belge <maroti@gmail.com>
2022-10-01 16:38:31 +0000

    yusuf

---
+++ my.java
@@ -0,0 +1,15 @@
+ip-172-31-43-241.ap-south-1.compute.internal
+.
+├── f1
+├── f12
+├── f12.txt
+├── f23
+├── k1
+├── k1.java
+├── kiran
+├── my.java
+├── t2.txt
+├── t3.txt
+└── touch.txt
+
+0 directories, 11 files
